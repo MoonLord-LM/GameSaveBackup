84dfca40d6d233feadbf1c4e93a9cf7bc0425336
MoonLord-LM <me@moonlord.cn>
2024-06-27 01:06:59 +0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Game-Save-Backup
+# GameSaveBackup
+A simple game save file backup tool, using git for file version control  
+
+# 游戏存档备份
+一个简单的游戏存档文件备份工具，使用 git 进行文件版本控制  
---
+++ zh-cn/游戏存档备份.bat
@@ -0,0 +1,45 @@
+@echo off
+setlocal enabledelayedexpansion
+chcp 65001
+
+:: jq-windows-amd64.exe
+:: 1.7.1
+:: https://jqlang.github.io/jq/download/
+
+
+
+if not exist ".git" (
+    git init
+    git config --local core.autocrlf false
+    git config --local core.safecrlf false
+    git config --local core.ignorecase false
+)
+
+set "config=config.json"
+for /f %%i in ('jq length "%config%"') do set "length=%%i"
+echo.
+
+for /l %%i in (1, 1, %length%) do (
+    for /f "tokens=*" %%j in ('jq -r ".[%%i - 1].game" "%config%"') do set "game=%%j"
+    for /f "tokens=*" %%j in ('jq -r ".[%%i - 1].save" "%config%"') do set "save=%%j"
+
+    set "save=!save:%%USERPROFILE%%=%USERPROFILE%!"
+    echo "progress %%i / !length!"  :  "!game!" in "!save!"
+
+    if not exist "!game!" ( mkdir "!game!" )
+    cd "!game!"
+    xcopy "!save!" . /E /I /Y
+    echo "explorer.exe" "!save!" > "OpenSave.bat"
+
+    git add .
+    git commit -m "Update - !game!"
+    cd ..
+    echo.
+)
+
+git add .
+git commit -m "Update"
+
+endlocal
+pause
+exit
